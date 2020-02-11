271a0dd5770289dd02c8c7aab7c633accb504d50
Teo <teo.chaban@mail.utoronto.ca>
2020-02-11 13:35:21 -0500

    All

---
+++ Geo/ConvexHull.cpp
@@ -0,0 +1,16 @@
+bool cmp(pt a, pt b){return mk(a.y, a.x) < mk(b.y, b.x);}
+
+vector<pt> convexhull(vector<pt> p){	//counterclockwise, no collinear points
+	sort(p.begin(), p.end(), cmp);
+	p.erase(unique(p.begin(), p.end()), p.end());
+	vector<pt> up, dn;
+	for(pt i : p){
+		while(up.size() > 1 and orient(up[up.size() - 2], up.back(), i) >= 0) up.pop_back();
+		while(dn.size() > 1 and orient(dn[dn.size() - 2], dn.back(), i) <= 0) dn.pop_back();
+		up.pb(i);
+		dn.pb(i);
+	}
+	for(int i = (int) up.size() - 2; i >= 1; i--)
+		dn.pb(up[i]);
+	return dn;
+}
