7fd7643108876a66b08868ef479da3684a3d1d12
Victor Tao <platers81@gmail.com>
2020-02-12 11:02:23 -0500

    updated math, not compiled yet

--- Math/math.cpp
+++ Math/math.cpp
@@ -52,4 +52,25 @@ However, the main definition is the following.
 
 \sum_{d|n} \mu (d) = 0 for all n>1, and \mu (n) is multiplicative.
 (A function f is multiplicative if f(mn)=f(m)f(n) for all (m,n)=1.)
+
+Enumberating submasks in O(3^n)
+for (int m=0; m<(1<<n); ++m)
+    for (int s=m; s; s=(s-1)&m)
+ ... s and m ...
+
+Catalan numbers
+The first few numbers Catalan numbers, Cn (starting from zero):
+1,1,2,5,14,42,132,429,1430,…
+C_n = (2n choose n)/(n+1)
+The Catalan number Cn is the solution for:
+Number of correct bracket sequence consisting of n opening and n closing brackets.
+The number of rooted full binary trees with n+1 leaves (vertices are not numbered). A rooted binary tree is full if every vertex has either two children or no children.
+The number of ways to completely parenthesize n+1 factors.
+The number of triangulations of a convex polygon with n+2 sides (i.e. the number of partitions of polygon into disjoint triangles by using the diagonals).
+The number of ways to connect the 2n points on a circle to form n disjoint chords.
+The number of non-isomorphic full binary trees with n internal nodes (i.e. nodes having at least one son).
+The number of monotonic lattice paths from point (0,0) to point (n,n) in a square lattice of size n×n, which do not pass above the main diagonal (i.e. connecting (0,0) to (n,n)).
+Number of permutations of length n that can be stack sorted (i.e. it can be shown that the rearrangement is stack sorted if and only if there is no such index i<j<k, such that ak<ai<aj ).
+The number of non-crossing partitions of a set of n elements.
+The number of ways to cover the ladder 1…n using n rectangles (The ladder consists of n columns, where ith column has a height i).
 */
