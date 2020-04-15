4fad5112840049ff512e5bdf6a63057c67d0785f
Victor Tao <platers81@gmail.com>
2020-04-15 14:22:41 -0400

    faster and cleaner dijkstra

--- Algorithms/dijkstras.cpp
+++ Algorithms/dijkstras.cpp
@@ -1,38 +1,18 @@
-#include <bits/stdc++.h>
-#include <utility>
-#define MAXN 505
-
-using namespace std;
-
-typedef long long ll;
-typedef pair<int, int> ii; 
-int n;
-
-vector<pair<int, int> > g[MAXN];
-int dist[MAXN];
-
-void dijkstra(int x){
-	for(int i = 0; i < n; i++){
-		dist[i] = 99999999;
-	}
-	priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
-	pq.push({0, x});
-	dist[x] = 0;
-	while(!pq.empty()){
-		pair<int, int> v = pq.top();
-		pq.pop();
-		for(int i = 0; i < g[v.second].size(); i++){
-			pair<int, int> u = g[v.second][i];
-			if(dist[v.second] + u.second < dist[u.first])
-				pq.push({dist[u.first] = dist[v.second] + u.second, u.first});
-		}
-	}
-}
-
-int main(){
-    #ifndef ONLINE_JUDGE
-		freopen("input.txt", "r", stdin);
-	#endif
-	ios_base::sync_with_stdio(false);
-    //cin >> n;
+vector<ll> dijkstra(vector<vector<pii>> &G, int v) {
+    vector<ll> ans(G.size() + 1, inf);
+    ans[v] = 0;
+    auto cmp = [&](int i, int j) { return ans[i] > ans[j]; };
+    priority_queue<int, vector<int>, decltype(cmp)> q(cmp);
+    q.push(v);
+    while(!q.empty()) {
+        auto f = q.top();
+        q.pop();
+        for(auto i : G[f]){
+            if (ans[i.fi] > ans[f] + i.se){
+            	ans[i.fi] = ans[f] + i.se;
+                q.push(i.fi);
+            }
+        }
+    }
+    return ans;
 }
